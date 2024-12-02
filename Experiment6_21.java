b2dbcbfd60ff0e69e21ca8cd7d2b9c235c277997
triaanandafh <fadiltria51@gmail.com>
2024-12-02 11:36:02 +0700

    changes

--- Experiment6_21.java
+++ Experiment6_21.java
@@ -1,5 +1,14 @@
 import java.util.Scanner;
 public class Experiment6_21 {
+    static int calculateArea (int l, int w){
+        int area = l*w;
+        return area;
+    }
+    static int calculateVolume (int l, int w, int h){
+        int vol = calculateArea(l, w)*h;
+        return vol;
+    }
+    
     public static void main(String[] args) {
         Scanner input21 = new Scanner(System.in);
         int p, l, t, L, vol;
